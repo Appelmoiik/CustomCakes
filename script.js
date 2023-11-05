9b15c236390eeab9eff3edb82789b2af7d95d656
appelmoik <davidojeze@gmail.com>
2023-11-06 00:29:23 +0100

    message

---
+++ script.js
@@ -0,0 +1,9 @@
+const mobileMenu = document.getElementById('mobile-menu');
+const menu = document.getElementById('menu');
+
+mobileMenu.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
+
+
+
